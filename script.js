54dc9ae547a75a228d492a5d867e59439cb8b387
davidmorenocs <davidmorenocs@gmail.com>
2025-07-04 10:51:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function showZoom(src) {
+  document.getElementById('zoomImage').src = src;
+  document.getElementById('zoomBox').style.display = 'block';
+}
+function hideZoom() {
+  document.getElementById('zoomBox').style.display = 'none';
+}
